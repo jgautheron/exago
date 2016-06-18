a6d8cee8c3557af182cd8a4cb832d39509d14eb5
Jonathan Gautheron <jgautheron@neverblend.in>
2016-06-18 02:05:39 +0200

    Use generated badge

--- badge/badge.go
+++ badge/badge.go
@@ -1,34 +1,23 @@
 package badge
 
 import (
-	"io/ioutil"
 	"net/http"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/narqo/go-badge"
 )
 
 const (
-	title = "exago"
+	Title = "exago"
 )
 
 func Write(w http.ResponseWriter, val, color string) {
-	// TODO: Should work directly instead on the raw SVG file
-	resp, err := http.Get("https://img.shields.io/badge/" + title + "-" + val + "-" + color + ".svg")
-	if err != nil {
-		log.Error(err)
-		WriteError(w)
-		return
-	}
-	defer resp.Body.Close()
-	img, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Error(err)
-		WriteError(w)
-		return
-	}
 	w.Header().Set("Content-Type", "image/svg+xml")
 	w.Header().Set("Cache-Control", "no-cache")
-	w.Write(img)
+	err := badge.Render(Title, val, badge.Color(color), w)
+	if err != nil {
+		log.Error(err)
+	}
 }
 
 func WriteError(w http.ResponseWriter) {
