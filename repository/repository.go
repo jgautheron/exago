c1c0c5266403bb5383347e02d5531af9692685ca
Jonathan Gautheron <jgautheron@users.noreply.github.com>
2016-11-02 15:36:32 +0100

    Use logrus

--- repository/repository.go
+++ repository/repository.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"time"
 
-	"github.com/go-xweb/log"
+	log "github.com/Sirupsen/logrus"
 	"github.com/hotolab/exago-svc/repository/model"
 	"github.com/hotolab/exago-svc/score"
 )
