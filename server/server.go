d0173d276fd3304e0d25fce61001c3ecf5ca06b5
Jonathan Gautheron <alpherz@gmail.com>
2016-12-07 22:40:36 +0100

    Switch to chi

--- server/server.go
+++ server/server.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"time"
 
-	"gopkg.in/tylerb/graceful.v1"
+	"github.com/tylerb/graceful"
 
-	_ "net/http/pprof"
+	"net/http"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/gorilla/context"
 	exago "github.com/hotolab/exago-svc"
 	. "github.com/hotolab/exago-svc/config"
-	"github.com/justinas/alice"
+	"github.com/pressly/chi"
+	"github.com/pressly/chi/middleware"
+	"github.com/pressly/valve"
 )
 
 var (
@@ -41,32 +42,61 @@ func New(options ...exago.Option) *Server {
 
 // ListenAndServe binds the HTTP port and listens for requests.
 func (s *Server) ListenAndServe() error {
-	repoHandlers := alice.New(
-		context.ClearHandler,
-		s.recoverHandler,
-		s.setLogger,
-		s.checkValidRepository,
-		// rateLimit,
-		// requestLock,
-	)
-	router := NewRouter()
+	// Our graceful valve shut-off package to manage code preemption and
+	// shutdown signaling.
+	valv := valve.New()
+	baseCtx := valv.Context()
 
-	router.Get("/project/*repository", repoHandlers.ThenFunc(s.repositoryHandler))
-	router.Get("/refresh/*repository", repoHandlers.ThenFunc(s.refreshHandler))
-	router.Get("/contents/*repository", repoHandlers.ThenFunc(s.fileHandler))
-	router.Get("/cached/*repository", repoHandlers.ThenFunc(s.cachedHandler))
-	router.Get("/badge/:type/*repository", repoHandlers.ThenFunc(s.badgeHandler))
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.CloseNotify)
 
-	baseHandlers := alice.New(
-		context.ClearHandler,
-		s.recoverHandler,
-	)
-	router.Get("/projects/recent", baseHandlers.ThenFunc(s.recentHandler))
-	router.Get("/projects/top", baseHandlers.ThenFunc(s.topHandler))
-	router.Get("/projects/popular", baseHandlers.ThenFunc(s.popularHandler))
+	r.Route("/repos", func(r chi.Router) {
+		r.Use(s.checkValidRepository)
+		r.Post("/", s.repositoryHandler)
+		r.Route("/:host|:owner|:repo/branches/:branch/goversion/:goversion", func(r chi.Router) {
+			r.Get("/", s.cachedHandler)
+			r.Get("/file/*path", s.fileHandler)
+			r.Get("/badges/:type", s.badgeHandler)
+		})
+	})
 
-	router.Get("/_health", baseHandlers.ThenFunc(s.healthHandler))
+	r.Get("/_health", s.healthHandler)
 
+	// router.Get("/project/*repository", repoHandlers.ThenFunc(s.repositoryHandler))
+	// router.Get("/refresh/*repository", repoHandlers.ThenFunc(s.refreshHandler))
+	// router.Get("/contents/*repository", repoHandlers.ThenFunc(s.fileHandler))
+	// router.Get("/cached/*repository", repoHandlers.ThenFunc(s.cachedHandler))
+	// router.Get("/badge/:type/*repository", repoHandlers.ThenFunc(s.badgeHandler))
+
+	// baseHandlers := alice.New(
+	// 	context.ClearHandler,
+	// 	s.recoverHandler,
+	// )
+	// router.Get("/projects/recent", baseHandlers.ThenFunc(s.recentHandler))
+	// router.Get("/projects/top", baseHandlers.ThenFunc(s.topHandler))
+	// router.Get("/projects/popular", baseHandlers.ThenFunc(s.popularHandler))
+
+	// r.Get("/_health", baseHandlers.ThenFunc(s.healthHandler))
+
+	srv := &graceful.Server{
+		Timeout: 20 * time.Second,
+		Server:  &http.Server{Addr: fmt.Sprintf("%s:%d", Config.Bind, Config.HttpPort), Handler: chi.ServerBaseContext(r, baseCtx)},
+	}
+	srv.BeforeShutdown = func() bool {
+		fmt.Println("shutting down..")
+		err := valv.Shutdown(srv.Timeout)
+		if err != nil {
+			fmt.Println("Shutdown error -", err)
+		}
+
+		// the app code has stopped here now, and so this would be a good place
+		// to close up any db and other service connections, etc.
+		return true
+	}
 	logger.Infof("Listening on port %d", Config.HttpPort)
-	return graceful.RunWithErr(fmt.Sprintf("%s:%d", Config.Bind, Config.HttpPort), 10*time.Second, router)
+	return srv.ListenAndServe()
 }
